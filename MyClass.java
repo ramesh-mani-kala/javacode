9aedf22d415773fcd07ed758457a810ec5601b4a
root <root@8083b509234d>
2025-01-27 14:34:41 +0530

    Pushed Successfully

--- MyClass.java
+++ MyClass.java
@@ -11,7 +11,7 @@ public class KeywordExample {
         
         // Variable declarations and initialization
         int num1 = 10;  // 'int' is a primitive data type for integer numbers
-        doubl num2 = 20.5;  // 'double' is a primitive data type for floating-point numbers
+        d num2 = 20.5;  // 'double' is a primitive data type for floating-point numbers
         
         // 'if' is a conditional statement
         if (num1 < num2) {
